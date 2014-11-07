45ef163e1584e3c56202c7a1308847214753687b
Kristian Tashkov <kristian.tashkov@gmail.com>
2014-11-08 01:21:28 +0200

    Added light flickering in torch

---
+++ Assets/Scripts/Light/LightFlickering.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlickering : MonoBehaviour {
+	public float flickering_chance = 0.001F;
+	public float flickering_duration = 1.5F;
+	
+	
+	void Update () {
+		if(Random.Range(0.0F, 1.0F) < flickering_chance) {
+			StartCoroutine("Flicker");
+		}
+	}
+	
+	private IEnumerator Flicker() {
+		light.enabled = false;
+		yield return new WaitForSeconds(flickering_duration);
+		light.enabled = true;
+	}
+}
