41bdfc005bc63b32e6b585b57322237e1ba3d748
Kristian Tashkov <kristian.tashkov@gmail.com>
2014-11-07 23:45:23 +0200

    Image effect assest and health implementation

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+	public double max_health = 100;
+	public double current_health;
+
+	public float dying_angle_step = 0.1F;
+	public float dying_angle = 30;
+
+	private bool is_dead;
+	void Start () {
+		is_dead = false;
+		current_health = max_health;
+	}
+
+	public void Update() {
+		if(is_dead) {
+			return;
+		}
+		if(Input.GetKeyDown(KeyCode.K)) {
+			TakeDamage(20);
+		}
+	}
+
+	public void TakeDamage(double damage) {
+		if(damage < 0) {
+			return;
+		}
+
+		current_health -= damage;
+		if(current_health <= 0) {
+			Die();
+		}
+	}
+
+	public void Die() {
+		if(is_dead) {
+			return;
+		}
+		is_dead = true;
+		this.GetComponent<CharacterController>().enabled = false;
+		this.GetComponent<CharacterMotor>().enabled = false;
+		this.GetComponent<MouseLook>().enabled = false;
+		var camera = GameObject.FindWithTag("MainCamera");
+		camera.GetComponent<MouseLook>().enabled = false;
+		camera.GetComponent<Blur>().enabled = true;
+		this.transform.position += new Vector3(0, -1, 0);
+		StartCoroutine("DyingCamera");
+	}
+
+	public IEnumerator DyingCamera() {
+		var camera = GameObject.FindWithTag("MainCamera");
+		for(float i = 0; i < dying_angle; i+= dying_angle_step) {
+			camera.transform.rotation = new Quaternion(camera.transform.rotation.x - dying_angle_step,
+		                                           camera.transform.rotation.y,
+		                                           camera.transform.rotation.z,
+		                                           camera.transform.rotation.w);
+			yield return null;
+		}
+	}
+}
