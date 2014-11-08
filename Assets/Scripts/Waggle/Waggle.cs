0cffcb3b80317b8fd173c9736536c48b51464179
snke07 <delqn.dimitrov@gmail.com>
2014-11-08 23:24:03 +0200

    Import waggle script for lamp and furniture models pack

---
+++ Assets/Scripts/Waggle/Waggle.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waggle : MonoBehaviour {
+	public float MIN_X_VALUE = -20;
+
+	public float MAX_X_VALUE = 20;
+
+	public float waggleSpeed = 1f;
+
+	public float rotationStep;
+
+	private bool isRight;
+
+	private bool changeDirection;
+
+	// Use this for initialization
+	void Start () {
+		rotationStep = waggleSpeed;
+		changeDirection = true;
+		isRight = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float rotation = gameObject.transform.rotation.eulerAngles.x;
+		if (rotation > 180) {
+			rotation = rotation - 360;
+		}
+
+		if ((rotation <= MIN_X_VALUE && !isRight) || (isRight && rotation >= MAX_X_VALUE)) {
+			rotationStep *= -1;
+			isRight = !isRight;
+		}
+
+		gameObject.transform.Rotate (rotationStep, 0, 0);
+	}
+}
