e0a05bd9f3e1139484ea39c454b0a4ce18729e99
fo <fo@jitsi.org>
2014-11-08 18:20:22 +0200

    Adds architecture textures pack to assets.

---
+++ Assets/Scripts/Interactable/Door.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : Interactable {
+
+	public bool isOpen = false;
+	public float angleToOpen = 70f;
+	public float timeToActive;
+	public float timeInactive = 1f;
+	public override void Update() {
+		if (timeToActive > 0) {
+			timeToActive -= Time.deltaTime;
+		}
+		base.Update ();
+	}
+	public override void Interact ()
+	{
+		if (timeToActive <= 0) {
+			var angle = isOpen ? angleToOpen : -angleToOpen;
+			this.gameObject.transform.Rotate (0, angle, 0);
+			timeToActive = timeInactive;
+		}
+	}
+}
