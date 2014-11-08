6d0f80048db74f1448d08c47b89afcfe8b9d3cc9
Kristian Tashkov <kristian.tashkov@gmail.com>
2014-11-08 11:29:00 +0200

    Tied up heartbeat audio with heart monitor

---
+++ Assets/Scripts/Player/HeartbeatSound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartbeatSound : MonoBehaviour {
+
+	private AudioClip heartbeat;
+	private const float audio_beats_per_minute = 120.0F;
+	private const float base_volume = 0.5F;
+
+	private HeartMonitor heart_monitor;
+
+	void Start() {
+		heart_monitor = GameObject.FindWithTag("Player").GetComponent<HeartMonitor>();
+	}
+
+	void Update () {
+		audio.pitch = heart_monitor.currentRate / audio_beats_per_minute;
+		audio.volume = base_volume * ((float)heart_monitor.currentRate / heart_monitor.baseLine);
+	}
+}
