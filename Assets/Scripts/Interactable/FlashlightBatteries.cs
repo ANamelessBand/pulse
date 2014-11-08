8d3c616f589681f7a3657827db19cba01bc97241
Kristian Tashkov <kristian.tashkov@gmail.com>
2014-11-08 17:57:44 +0200

    Fix compiler errors

--- Assets/Scripts/Interactable/FlashlightBatteries.cs
+++ Assets/Scripts/Interactable/FlashlightBatteries.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class FlashlightBatteries : Interactable {
 
 	public override void Interact() {
-		var flashlight = GameObject.FindWithTag ("Flashlight").GetComponent<Flashlight>;
+		var flashlight = GameObject.FindWithTag ("Flashlight").GetComponent<Flashlight>();
 		if (flashlight.hasFlashlight) {
 			flashlight.refreshBatteries();
 			Destroy (this.gameObject);
--- Assets/Scripts/Light/Flashlight.cs
+++ Assets/Scripts/Light/Flashlight.cs
@@ -6,8 +6,8 @@ public class Flashlight : MonoBehaviour {
 	public bool hasFlashlight;
 	public float maxBatteryPower = 40;
 	public float batteryPower;
-	public float maxIntensity = 0.5;
-	public float minIntensity = 0.15;
+	public float maxIntensity = 0.5F;
+	public float minIntensity = 0.15F;
 	public bool flickerEnabled;
 
 	// Use this for initialization
@@ -19,8 +19,8 @@ public class Flashlight : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(hasFlashlight && batteryPower > 0) {
-			var light = this.gameObject.GetComponent<Light>;
-			light.enable = true;
+			var light = this.gameObject.GetComponent<Light>();
+			light.enabled = true;
 			light.intensity = minIntensity + (batteryPower / maxBatteryPower) * (maxIntensity - minIntensity);
 			batteryPower--;
 		}
