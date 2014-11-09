250b0c067c86287af6981149e9ad6bed402212be
Kristian Tashkov <kristian.tashkov@gmail.com>
2014-11-09 15:24:31 +0200

    Added light controller to control flickering

---
+++ Assets/Scripts/Light/LightController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightController : MonoBehaviour {
+	private LightFlickering light_flickering;
+	public bool is_disabled = false;
+	void Start () {
+		light_flickering = this.gameObject.GetComponent<LightFlickering>();
+	}
+	
+	void Update () {
+		if (!is_disabled && !light_flickering.is_flickering) {
+			this.gameObject.light.enabled = true;
+		} else {
+			this.gameObject.light.enabled = false;
+		}
+	}
+}
